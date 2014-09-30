60eca769d75b0289d80b32150cb391828c968522
subutan <naoya.shiba310@gmail.com>
2014-09-30 18:04:03 +0900

    top igai finish

--- public_html/js/scrollmenu.js
+++ public_html/js/scrollmenu.js
@@ -58,23 +58,3 @@ function noselectOpacity(element) {
 	$(element).removeClass('select');
 	$(element).addClass('noselect');
 }
-
-$(document).ready(function() {
-	$("#back-top").hide();
-
-	$(function() {
-		$(window).scroll(function() {
-			if ($(this).scrollTop() > 1000) {
-				$('#back-top').fadeIn();
-			} else {
-				$('#back-top').fadeOut();
-			}
-		});
-		$('#back-top a').click(function() {
-			$('body,html').animate({
-				scrollTop: 0
-			}, 400);
-			return false;
-		});
-	});
-});
