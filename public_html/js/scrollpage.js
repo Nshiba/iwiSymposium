5371bded146e150046a29bebdd9c901d9b280ab4
subutan <naoya.shiba310@gmail.com>
2014-10-06 18:46:51 +0900

    top henkou

--- public_html/js/scrollpage.js
+++ public_html/js/scrollpage.js
@@ -1,6 +1,19 @@
 $(document).ready(function (){
-	$('#page').pageswich({
-		properties: { marginLeft: -$('body').width() },
-		options: { duration: 1000 }
+	$("#back-top").hide();
+
+	$(function() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 500) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+		$('#back-top a').click(function() {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 700);
+			return false;
+		});
 	});
 });
